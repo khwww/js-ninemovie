be9fa6f56556741104215e6257ed91d21a294e7c
Je <song2805@empas.com>
2024-07-16 10:54:26 -0700

    fetch & compiled json commit

--- recommendation.js
+++ recommendation.js
@@ -1,13 +1,18 @@
 const API_KEY = `af4e249f208fd13fc96bb460a631d94a`;
 let popularMovieList = [];
 
+
 const getPopularMovie = async() => {
-    url = new URL (
+    const url = new URL (
         `https://api.themoviedb.org/3/movie/popular?language=kr-KO&page=1&api_key=${API_KEY}`
     )   
-    console.log("uuu", url);
+    // console.log("uuu", url);
+    
 
     const response = await fetch(url);
+    const popularData = await response.json();
+    console.log("rrr",popularData.results);
 };
+
 
     getPopularMovie();
