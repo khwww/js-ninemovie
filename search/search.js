bd076ea72ccf087fd1f2956920348b874fb9bb8b
victoryalhj <victoryalhj@gmail.com>
2024-07-17 23:42:39 +0100

    getMoviesCategory 문제

--- search/search.js
+++ search/search.js
@@ -1,59 +1,10 @@
 // const API_key = config.apikey;
-const API_KEY = `api_key=bd09ec96d68f7f24d24f00a9149d0e0a`;
-const BASE_URL = `https://api.themoviedb.org/3/`;
-const API_URL = BASE_URL + `discover/movie?sort_by=popularity.desc&`+ API_KEY;
-const IMG_URL = `https://image.tmdb.org/t/p/w500`;
-const categorySlide = document.getElementById('category-slide');
+// const API_KEY = `api_key=bd09ec96d68f7f24d24f00a9149d0e0a`;
+// const BASE_URL = `https://api.themoviedb.org/3/`;
+// const API_URL = BASE_URL + `discover/movie?sort_by=popularity.desc&`+ API_KEY;
+// const IMG_URL = `https://image.tmdb.org/t/p/w500`;
+// const searchURL = BASE_URL + '/search/movie?' + API_KEY;
 
-getMovies(API_URL);
-
-function getMovies(url) {
-  fetch(url).then(response => response.json()).then(data => {
-    console.log(data.results);
-    showMovies(data.results);
-  })
-}
-
-function showMovies(data) {
-  categorySlide.innerHTML = '<div class="swiper-wrapper"></div>';
-  const swiperWrapper = categorySlide.querySelector('.swiper-wrapper');
-
-  data.forEach(movie => {
-    const {title, poster_path} = movie;
-    const movieEl = document.createElement('div');
-    movieEl.classList.add('swiper-slide', 'movie');
-    movieEl.innerHTML = `
-        <img src="${IMG_URL+poster_path}" alt="${title}">${title}
-        `
-        swiperWrapper.appendChild(movieEl);
-  });
-  initializeSwiper();
-}
-
-
-function initializeSwiper() {
-  const slides = document.querySelectorAll('.swiper-container .swiper-slide');
-  const slidesCount = slides.length;
-
-  let slidesPerView = 10; //한번에 보여줄 슬라이드 개수
-  let slidesPerGroup = 1; //한번에 이동할 슬라이드 개수
-
-  if (slidesCount < slidesPerView) {
-    slidesPerView = slidesCount;
-    slidesPerGroup = slidesCount;
-  }
-
-  new Swiper('.swiper-container', {
-    slidesPerView: slidesPerView,
-    slidesPerGroup: slidesPerGroup,
-    spaceBetween: 10,
-    loop: slidesCount >= slidesPerView,
-    navigation: {
-      prevEl: '.swiper-icon-left',
-      nextEl: '.swiper-icon-right'
-    }
-  });
-}
 
 
 //NavBar  
@@ -65,3 +16,63 @@ const closeNav = () => {
   document.getElementById("topnav").style.display = 'none';
 }
 
+
+
+const API_KEY = `bd09ec96d68f7f24d24f00a9149d0e0a`;
+const IMG_URL = `https://image.tmdb.org/t/p/w500`;
+let moviesList = []; //result 안에 title있음
+const menus = document.querySelectorAll(".menus button");
+menus.forEach(menu=>menu.addEventListener("click",(event)=>getMoviesCategory(event)))
+
+
+const getMovies = async () => {
+  const url = new URL(
+    `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en`
+  );
+ const response = await fetch(url);
+ const data = await response.json();
+ moviesList = data.results; 
+ render();
+ console.log("response", moviesList);
+};
+
+const getMoviesCategory = async (event) => {
+  const category = event.target.textContent.toLowerCase();
+  let categoryUrl;
+
+  switch (category) {
+    case '인기':
+      categoryUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en`;
+      break;
+    case '최신작':
+      categoryUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en`;
+      break;
+    default:
+      categoryUrl = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en`;
+  }
+
+    try {
+    const response = await fetch(categoryUrl);
+    const data = await response.json();
+    moviesList = data.results;
+    render();
+    console.log("Response:", moviesList);
+  } catch (error) {
+    console.error('Error fetching movies:', error);
+  }
+};
+
+const render = () => {
+  const moviesHTML = moviesList.map(
+    (movies) => ` <div class="swiper-wrapper">
+                    <div class="swiper-slide_1 movie">
+                      <img src=${IMG_URL + movies.poster_path}>
+                    </div>
+                  </div>`
+      ).join('');
+
+  document.getElementById("movie-search-board").innerHTML = moviesHTML
+}
+
+
+getMovies();
