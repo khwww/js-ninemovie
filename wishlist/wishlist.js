5f6cbc429f8ccd3e1a1bea384ed93031b2081e11
Seongsuk Hong <ssukh77@naver.com>
2024-07-18 18:18:22 +0900

    feat: 영화 상세 데이터로 변경, 찜 삭제 기능 추가
    - 영화 상세 데이터에 맞춰 코드 수정
    - 찜 여부에 따라 아이콘 변경
    - 찜 삭제 기능 추가
    - 재로드를 위해 영화 데이터 그대로 내려받도록 수정

--- wishlist/wishlist.js
+++ wishlist/wishlist.js
@@ -1,7 +1,7 @@
 // 찜한 영화 데이터 가져오는 함수
 const getWishlist = () => {
   const wishlist = localStorage.getItem("wishlist");
-  console.log("로컬스토리지에서 저장된 찜한 데이터", wishlist);
+  console.log("로컬스토리지에서 저장된 찜한 데이터", JSON.parse(wishlist));
   return wishlist ? JSON.parse(wishlist) : [];
 };
 
@@ -12,14 +12,13 @@ const wishlistRender = () => {
   if (wishlist.length > 0) {
     const wishlistHTML = wishlist
       .map(
-        (movie) =>
-          `
+        (movie) => `
         <li class="wishlist-item">
           <a href="#">
             <div class="wishlist-item-img">
               <img
                 class="img-fluid rounded"
-                src="https://image.tmdb.org/t/p/original/${movie.image}"
+                src="https://image.tmdb.org/t/p/original/${movie.poster_path}"
                 alt="${movie.title}"
               />
             </div>
@@ -35,4 +34,5 @@ const wishlistRender = () => {
     document.getElementById("wishlist").innerHTML = wishlistHTML;
   }
 };
+
 wishlistRender();
