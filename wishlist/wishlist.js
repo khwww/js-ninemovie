cd949018d4a13ec387cdd9426388bacc60d06823
Seongsuk Hong <ssukh77@naver.com>
2024-07-19 17:42:01 +0900

    feat: 찜 목록 카테고리별 필터링 기능 추가

--- wishlist/wishlist.js
+++ wishlist/wishlist.js
@@ -1,13 +1,21 @@
+const defaultCategory = "시리즈";
+
+const categoryButton = document.querySelectorAll("#wishlist-category button");
+
+categoryButton.forEach((button) => {
+  button.addEventListener("click", categorySearch);
+});
+
 // 찜한 영화 데이터 가져오는 함수
-const getWishlist = () => {
+function getWishlist() {
   const wishlist = localStorage.getItem("wishlist");
   console.log("로컬스토리지에서 저장된 찜한 데이터", JSON.parse(wishlist));
   return wishlist ? JSON.parse(wishlist) : [];
-};
+}
 
 // 찜 리스트 렌더링
-const wishlistRender = () => {
-  const wishlist = getWishlist();
+const wishlistRender = (wishlist) => {
+  console.log("wishlistRender", wishlist);
 
   if (wishlist.length > 0) {
     const wishlistHTML = wishlist
@@ -35,4 +43,42 @@ const wishlistRender = () => {
   }
 };
 
-wishlistRender();
+// 초기 로드
+const initializeView = (category) => {
+  const wishlist = categoryFilter(category);
+  wishlistRender(wishlist);
+};
+
+initializeView(defaultCategory);
+
+// 카테고리별 찜 리스트
+function categorySearch(e) {
+  categoryButton.forEach((button) => {
+    button.classList.remove("active");
+  });
+  e.target.classList.add("active");
+
+  const category = e.target.textContent;
+  const wishlist = categoryFilter(category);
+
+  wishlistRender(wishlist);
+}
+
+// 카테고리별 필터링
+function categoryFilter(category) {
+  const wishlist = getWishlist();
+
+  let filterWishlist = [];
+  // 카테고리별 데이터 나누기
+  if (category === "시리즈") {
+    filterWishlist = wishlist.filter(
+      (movie) => movie.belongs_to_collection !== null
+    );
+  } else if (category === "영화") {
+    filterWishlist = wishlist.filter(
+      (movie) => movie.belongs_to_collection === null
+    );
+  }
+
+  return filterWishlist;
+}
