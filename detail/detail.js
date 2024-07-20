25c5aff6f81a8fe4639171f71462b140e50ea7b8
victoryalhj <victoryalhj@gmail.com>
2024-07-20 17:41:05 +0100

    Merge branch 'develop' into feature-lhj 0720

---
+++ detail/detail.js
@@ -0,0 +1,273 @@
+const urlParams = new URLSearchParams(window.location.search);
+const movieId = urlParams.get("id");
+
+const detail_apiKey = "4e4f177acb805afedb5f5f64d33de73d";
+const youtubeApiKey = "AIzaSyCQKzBO4AtZ0_Fk7ViMJYWp1Ci2qzoQ8P4";
+// const movieId = 557;
+
+async function fetchMovieDetails() {
+  const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${detail_apiKey}&language=ko-KR`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayMovieDetails(data);
+    fetchSimilarMovies(movieId);
+    fetchMovieCast(movieId); // 출연진 정보를 가져오는 함수 호출
+  } catch (error) {
+    console.error("Error fetching movie details:", error);
+  }
+}
+
+function displayMovieDetails(movie) {
+  document.getElementById("movieTitle").textContent = movie.title;
+  document.getElementById(
+    "movieRating"
+  ).textContent = `평점: ${movie.vote_average}`;
+  document.getElementById(
+    "releaseDate"
+  ).textContent = `개봉일: ${movie.release_date}`;
+  document.getElementById("movieCategory").textContent = movie.genres
+    .map((genre) => genre.name)
+    .join(", ");
+  document.getElementById(
+    "movieRuntime"
+  ).textContent = `러닝타임: ${movie.runtime}분`;
+  document.getElementById("movieOverview").textContent = movie.overview;
+  document.getElementById(
+    "moviePoster"
+  ).src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+  // 감독 정보 추가
+  fetchMovieCredits(movie.id);
+
+  // 영화 제목으로 유튜브 영상 검색
+  searchYouTube(movie.title);
+
+  // 찜하기
+  displayLike(movie);
+}
+
+async function fetchMovieCredits(movieId) {
+  const url = `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${detail_apiKey}&language=ko-KR`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayMovieCrew(data.crew);
+    displayMovieCast(data.cast); // 출연진 정보 표시 함수 호출
+  } catch (error) {
+    console.error("Error fetching movie credits:", error);
+  }
+}
+
+function displayMovieCrew(crew) {
+  const director = crew.find((member) => member.job === "Director");
+  if (director) {
+    document.getElementById("movieCrew").innerHTML = `감독: ${director.name}`;
+  }
+}
+
+// 주요 출연진 정보를 가져오는 함수
+async function fetchMovieCast(movieId) {
+  const url = `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${detail_apiKey}&language=ko-KR`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log("Movie Cast API response:", data); // 로그 추가
+    displayMovieCast(data.cast);
+  } catch (error) {
+    console.error("Error fetching movie cast:", error);
+  }
+}
+
+// 주요 출연진 정보를 표시하는 함수
+function displayMovieCast(cast) {
+  const castContainer = document.getElementById("movieCast");
+  const castNames = cast
+    .slice(0, 5)
+    .map((member) => member.name)
+    .join(", ");
+  castContainer.innerHTML = `출연: ${castNames}`;
+}
+
+// 관련 콘텐츠를 가져오는 함수
+async function fetchSimilarMovies(movieId) {
+  const url = `https://api.themoviedb.org/3/movie/${movieId}/similar?api_key=${detail_apiKey}&language=ko-KR`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log("Similar Movies API response:", data); // 로그 추가
+    const limitedResults = data.results.slice(0, 6);
+    displaySimilarMovies(limitedResults);
+  } catch (error) {
+    console.error("Error fetching similar movies:", error);
+  }
+}
+
+function displaySimilarMovies(movies) {
+  const similarMoviesContainer = document.getElementById("similarMovies");
+  similarMoviesContainer.innerHTML = "";
+
+  if (movies.length === 0) {
+    similarMoviesContainer.innerHTML = "<p>추천할 콘텐츠가 없습니다.</p>";
+    return;
+  }
+
+  movies.forEach((movie) => {
+    const movieElement = document.createElement("div");
+    movieElement.classList.add("similar-movie");
+
+    movieElement.innerHTML = `
+      <img src="https://image.tmdb.org/t/p/w200${movie.poster_path}" alt="${movie.title}"
+      data-id="${movie.id}">
+
+    `;
+
+    movieElement.addEventListener("click", () => {
+      window.location.href = `detail.html?id=${movie.id}`;
+    });
+
+    similarMoviesContainer.appendChild(movieElement);
+  });
+}
+
+// maxResults인자를 통해서 몇 개의 영상을 가져올 것인지를 결정
+async function searchYouTube(query) {
+  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=4&q=${encodeURIComponent(
+    query
+  )} trailer&key=${youtubeApiKey}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log("YouTube API response:", data); //로그확인
+    displayVideos(data.items);
+  } catch (error) {
+    console.error("Error fetching YouTube data:", error);
+    document.getElementById("videoResults").innerHTML =
+      "YouTube 데이터를 가져오는 중 오류가 발생했습니다.";
+  }
+}
+
+function displayVideos(items) {
+  const videoResults = document.getElementById("videoResults");
+  videoResults.innerHTML = "";
+
+  items.forEach((item) => {
+    const videoId = item.id.videoId;
+    if (!videoId) {
+      console.warn("No videoId found for item:", item); // 경고 로그 추가
+      return;
+    }
+    const iframe = document.createElement("iframe");
+    iframe.src = `https://www.youtube.com/embed/${videoId}`;
+    iframe.allow =
+      "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+    iframe.allowFullscreen = true;
+    videoResults.appendChild(iframe);
+  });
+}
+
+let currentPosition = 0;
+const slideWidth = 220; // 포스터 너비 + 마진
+const visibleSlides = 5;
+
+function slideLeft() {
+  const slider = document.getElementById("similarMovies");
+  currentPosition = Math.min(currentPosition + slideWidth * visibleSlides, 0);
+  slider.style.transform = `translateX(${currentPosition}px)`;
+}
+
+function slideRight() {
+  const slider = document.getElementById("similarMovies");
+  const maxPosition = -(slider.scrollWidth - slider.clientWidth);
+  currentPosition = Math.max(
+    currentPosition - slideWidth * visibleSlides,
+    maxPosition
+  );
+  slider.style.transform = `translateX(${currentPosition}px)`;
+}
+
+fetchMovieDetails();
+
+/**
+ * 찜하기
+ */
+
+// 찜한 영화 데이터 가져오기
+const getWishlist = () => {
+  const wishlist = localStorage.getItem("wishlist");
+  console.log("로컬스토리지에서 저장된 찜한 데이터", JSON.parse(wishlist));
+  return wishlist ? JSON.parse(wishlist) : [];
+};
+
+// 로컬스토리지에 찜 저장
+const saveWishlist = (wishlist) => {
+  localStorage.setItem("wishlist", JSON.stringify(wishlist));
+};
+
+const displayLike = (movie) => {
+  // 찜한 찜한 영화 데이터 가져오기
+  const wishlist = getWishlist();
+  // 찜 여부 확인
+  const liked = wishlist.some((item) => item.id === movie.id);
+
+  // 찜 여부에 따라 표시
+  let likeButtonHTML = ``;
+  if (!liked) {
+    likeButtonHTML = `
+      <button id="like-button" type="button" onclick='addToWishlist(${JSON.stringify(
+        movie
+      )})'>
+        <i class="fa-regular fa-heart"></i>
+      </button>
+      `;
+  } else {
+    likeButtonHTML = `
+      <button id="like-button" type="button" onclick='removeFromWishlist(${JSON.stringify(
+        movie
+      )})'>
+        <i class="fa-solid fa-heart"></i>
+      </button>
+      `;
+  }
+
+  document.getElementById("like-button-area").innerHTML = likeButtonHTML;
+};
+
+// 찜하기
+// 찜 버튼을 누르면 찜 표시 - 로컬스토리지에 저장
+// 찜이 되어있는지 체크 - 로컬스토리지에 저장되어있는지 확인
+// 찜인 상태에서 클릭 시 찜 삭제와 표시 - 로컬스토리지 삭제
+
+// 찜 목록에 추가
+const addToWishlist = (movie) => {
+  // 찜한 찜한 영화 데이터 가져오기
+  const wishlist = getWishlist();
+
+  // 찜이 되어있는지 않는 경우 추가
+  const liked = wishlist.some((item) => item.id === movie.id);
+  if (!liked) {
+    wishlist.push(movie); // 찜 목록에 추가
+    console.log("찜 목록에 추가: ", wishlist);
+
+    // 로컬스토리지에 찜 목록 문자열로 저장
+    saveWishlist(wishlist);
+
+    displayLike(movie);
+  } else {
+    console.log("이미 찜이 되어있습니다.");
+  }
+};
+
+// 찜 삭제
+const removeFromWishlist = (movie) => {
+  let wishlist = getWishlist();
+  wishlist = wishlist.filter((item) => item.id !== movie.id);
+  saveWishlist(wishlist);
+
+  displayLike(movie);
+};
---
+++ wishlist/wishlist.js
@@ -0,0 +1,261 @@
+let category = "시리즈";
+let isEditMode = false;
+let isAllSelected = false;
+let selectedListId = [];
+let selectedMoviesCount = 0;
+
+const categoryButtons = document.querySelectorAll("#wishlist-category button");
+const editButton = document.getElementById("wishlist-edit-button");
+const editInner = document.getElementById("wishlist-edit-inner");
+const selectAllButton = document.getElementById("select-all-button");
+const deleteButton = document.getElementById("wishlist-delete-button");
+const completeButton = document.getElementById("wishlist-complete-button");
+
+// 카테고리 버튼들
+categoryButtons.forEach((button) => {
+  button.addEventListener("click", categorySearch);
+});
+
+// 편집 버튼
+editButton.addEventListener("click", editMode);
+
+// 전체선택
+selectAllButton.addEventListener("click", selectAllWishlist);
+
+// 삭제 버튼
+deleteButton.addEventListener("click", removeFromWishlist);
+
+// 완료 버튼
+completeButton.addEventListener("click", completeEditMode);
+
+// a 태그의 기본 동작을 막기
+function preventLink(event) {
+  event.preventDefault();
+}
+
+// 찜한 영화 데이터 가져오기
+const getWishlist = () => {
+  const wishlist = localStorage.getItem("wishlist");
+  console.log("로컬스토리지에서 저장된 찜한 데이터", JSON.parse(wishlist));
+  return wishlist ? JSON.parse(wishlist) : [];
+};
+
+// 로컬스토리지에 찜 목록 저장
+const saveWishlist = (wishlist) => {
+  localStorage.setItem("wishlist", JSON.stringify(wishlist));
+};
+
+// 찜 리스트 렌더링
+const wishlistRender = (wishlist, category) => {
+  console.log("wishlistRender", wishlist);
+
+  if (wishlist.length > 0) {
+    const wishlistHTML = wishlist
+      .map(
+        (movie) => `
+        <li id="${movie.id}" class="wishlist-item" onclick="selectWishlist(event, ${movie.id})">
+          <a href="../detail/detail.html?id=${movie.id}">
+            <div class="wishlist-item-thumbnail">
+              <div class="wishlist-item-img">
+                <img
+                  class="img-fluid rounded"
+                  src="https://image.tmdb.org/t/p/original/${movie.poster_path}"
+                  alt="${movie.title}"
+                />
+              </div>
+              <div id="wishlist-edit-mode">
+                <button id="wishlist-checkbox"><i class="fa-solid fa-check"></i></button>
+              </div>
+            </div>
+            <div class="wishlist-item-info">
+              <p class="wishlist-item-title">${movie.title}</p>
+            </div>
+          </a>
+        </li>
+      `
+      )
+      .join("");
+
+    document.getElementById("wishlist").innerHTML = wishlistHTML;
+    document.getElementById("emptyWishlist").style.display = "none";
+    editButton.style.display = "block";
+  } else {
+    document.getElementById("wishlist").innerHTML = "";
+    document.getElementById("emptyWishlist").style.display = "flex";
+    document.getElementById(
+      "emptyWishlist-content"
+    ).textContent = `찜한 ${category}가 없습니다.`;
+    editButton.style.display = "none";
+  }
+};
+
+// 초기 로드
+const initializeView = (category) => {
+  console.log(category);
+  const wishlist = categoryFilter(category);
+  wishlistRender(wishlist, category);
+};
+
+initializeView(category);
+
+// 카테고리별 찜 리스트
+function categorySearch(event) {
+  categoryButtons.forEach((button) => {
+    button.classList.remove("active");
+  });
+  event.target.classList.add("active");
+
+  category = event.target.textContent;
+  const wishlist = categoryFilter(category);
+
+  completeEditMode(); // 이전 편집 모드 초기화
+
+  wishlistRender(wishlist, category);
+}
+
+// 카테고리별 필터링
+function categoryFilter(category) {
+  const wishlist = getWishlist();
+
+  let filterWishlist = [];
+  // 카테고리별 데이터 나누기
+  if (category === "시리즈") {
+    filterWishlist = wishlist.filter(
+      (movie) => movie.belongs_to_collection !== null
+    );
+  } else if (category === "영화") {
+    filterWishlist = wishlist.filter(
+      (movie) => movie.belongs_to_collection === null
+    );
+  }
+
+  return filterWishlist;
+}
+
+// 편집 모드
+function editMode() {
+  isEditMode = true;
+
+  this.style.display = "none";
+  editInner.style.display = "flex";
+
+  const wishlistItems = document.querySelectorAll("#wishlist .wishlist-item");
+  wishlistItems.forEach((item) => {
+    // a 태그의 기본 동작을 막기 위해 이벤트 리스너 추가
+    item.querySelector("a").addEventListener("click", preventLink, true);
+    item.querySelector("#wishlist-edit-mode").style.display = "block";
+  });
+}
+
+// 전체 선택 / 해제하기
+function selectAllWishlist(event) {
+  const wishlistItems = document.querySelectorAll(".wishlist-item");
+
+  // 전체 선택 여부
+  isAllSelected = Array.from(wishlistItems).every((item) =>
+    item.querySelector("#wishlist-checkbox").classList.contains("selected")
+  );
+
+  if (!isAllSelected) {
+    event.target.textContent = "전체해제";
+
+    wishlistItems.forEach((item) => {
+      const movieId = parseInt(item.id);
+      const checkbox = item.querySelector("#wishlist-checkbox");
+
+      if (!checkbox.classList.contains("selected")) {
+        checkbox.classList.add("selected");
+
+        selectedListId.push(movieId);
+        console.log(selectedListId);
+        selectedMoviesCount++;
+      }
+    });
+
+    deleteButton.textContent = `삭제(${selectedMoviesCount})`;
+  } else {
+    event.target.textContent = "전체선택";
+
+    wishlistItems.forEach((item) => {
+      item.querySelector("#wishlist-checkbox").classList.remove("selected");
+    });
+
+    selectedListId = [];
+    console.log(selectedListId);
+    selectedMoviesCount = 0;
+    deleteButton.textContent = `삭제(${selectedMoviesCount})`;
+  }
+}
+
+// 찜 리스트 선택하기
+const selectWishlist = (event, movieId) => {
+  if (isEditMode === true) {
+    console.log(event.currentTarget);
+    const checkbox = event.currentTarget.querySelector("#wishlist-checkbox");
+
+    if (!checkbox.classList.contains("selected")) {
+      checkbox.classList.add("selected");
+
+      selectedListId.push(movieId);
+      console.log(selectedListId);
+
+      selectedMoviesCount++;
+      deleteButton.textContent = `삭제(${selectedMoviesCount})`;
+    } else {
+      checkbox.classList.remove("selected");
+
+      selectedListId = selectedListId.filter((num) => num !== movieId);
+      console.log(selectedListId);
+
+      selectedMoviesCount--;
+      deleteButton.textContent = `삭제(${selectedMoviesCount})`;
+    }
+
+    // 전체 선택 상태 업데이트
+    const wishlistItems = document.querySelectorAll("#wishlist .wishlist-item");
+    isAllSelected = Array.from(wishlistItems).every((item) =>
+      item.querySelector("#wishlist-checkbox").classList.contains("selected")
+    );
+    if (isAllSelected) {
+      selectAllButton.textContent = "전체해제";
+    } else {
+      selectAllButton.textContent = "전체선택";
+    }
+  }
+};
+
+// 찜 삭제
+function removeFromWishlist() {
+  const wishlist = getWishlist();
+
+  const newWishlist = wishlist.filter(
+    (item) => !selectedListId.includes(item.id)
+  );
+
+  saveWishlist(newWishlist); // 로컬스토리지 저장
+  console.log(newWishlist);
+
+  completeEditMode();
+  initializeView(category); // UI반영
+}
+
+// 편집 모드 완료
+function completeEditMode() {
+  isEditMode = false;
+  isAllSelected = false;
+  selectedListId = [];
+  selectedMoviesCount = 0;
+
+  selectAllButton.textContent = "전체선택";
+  deleteButton.textContent = `삭제(${selectedMoviesCount})`;
+
+  editInner.style.display = "none";
+  editButton.style.display = "block";
+
+  const wishlistItems = document.querySelectorAll("#wishlist .wishlist-item");
+  wishlistItems.forEach((item) => {
+    item.querySelector("a").removeEventListener("click", preventLink, true);
+    item.querySelector("#wishlist-checkbox").classList.remove("selected");
+    item.querySelector("#wishlist-edit-mode").style.display = "none";
+  });
+}
